8cf523e6abc92ff74ff7e0836a69fbee0d25eff1
Sven Baumann <baumann.sv@gmail.com>
2017-06-02 05:23:41 +0200

    new phpunit test class for \Gedmo\Timestampable\Mapping\Driver\ContaoDca

---
+++ tests/Test/Driver/ContaoDcaTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-doctrine-orm-timestampable.
+ *
+ * (c) 2013-2017 Contao Black Forest.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-doctrine-orm-timestampable
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2013-2017 Contao Community Alliance.
+ * @license    https://github.com/ContaoBlackForest/contao-doctrine-orm-timestampable/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace Gedmo\Timestampable\Mapping\Test\Driver;
+
+use Contao\Doctrine\ORM\Test\BaseTestCase;
+
+/**
+ * Test class for \Gedmo\Timestampable\Mapping\Driver\ContaoDca
+ */
+class ContaoDcaTest extends BaseTestCase
+{
+
+    public function test()
+    {
+    }
+}
