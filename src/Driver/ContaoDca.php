40dcdde8b70d2da890f00a1e2783b5df6c83daf8
Sven Baumann <baumann.sv@gmail.com>
2017-06-06 14:31:33 +0200

    ignore not test able method from coverage

--- src/Driver/ContaoDca.php
+++ src/Driver/ContaoDca.php
@@ -69,6 +69,8 @@ class ContaoDca implements Driver
      *
      * @param string $tableName The table name.
      *
+     * @codeCoverageIgnore
+     *
      * @return void
      */
     protected function loadDataContainer($tableName)
