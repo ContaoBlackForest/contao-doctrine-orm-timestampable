558a30d9652d0988918de9bf74fcf070777f55a0
Sven Baumann <baumann.sv@gmail.com>
2017-06-06 14:32:43 +0200

    extend tests for contao dca

--- tests/Test/Driver/ContaoDcaTest.php
+++ tests/Test/Driver/ContaoDcaTest.php
@@ -19,7 +19,6 @@
 
 namespace Gedmo\Timestampable\Mapping\Test\Driver;
 
-use Contao\Doctrine\ORM\Mapping\Driver\ContaoDcaDriver;
 use Contao\Doctrine\ORM\Test\BaseTestCase;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Gedmo\Timestampable\Mapping\Driver\ContaoDca;
@@ -32,23 +31,94 @@ class ContaoDcaTest extends BaseTestCase
     /**
      * Test if find property timestamp able who has create on.
      *
-     * @covers \Gedmo\Timestampable\Mapping\Driver\ContaoDca::__construct
      * @covers \Gedmo\Timestampable\Mapping\Driver\ContaoDca::readExtendedMetadata
+     * @covers \Gedmo\Timestampable\Mapping\Driver\ContaoDca::loadDataContainer
      */
     public function testReadExtendedMetadata()
     {
-        $this->aliasContaoClass('Controller');
+        $driver1 = $this->mockForContaoDriver();
+        $driver2 = $this->mockForContaoDriver();
+        $driver3 = $this->mockForContaoDriver(true);
+        $driver4 = $this->mockForContaoDriver(true);
+        $driver5 = $this->mockForContaoDriver(true);
+        $driver6 = $this->mockForContaoDriver(true);
+        $driver7 = $this->mockForContaoDriver(true);
+        $driver8 = $this->mockForContaoDriver(true);
+        $driver9 = $this->mockForContaoDriver(true);
 
-        $contaoDca = new ContaoDca();
+        $meta1 = new ClassMetadata('foo');
+        $meta2 = new ClassMetadata('orm_version');
+        $meta3 = new ClassMetadata('orm_version');
+        $meta4 = new ClassMetadata('orm_version');
+        $meta5 = new ClassMetadata('orm_version');
+        $meta6 = new ClassMetadata('orm_version');
+        $meta7 = new ClassMetadata('orm_version');
+        $meta8 = new ClassMetadata('orm_version');
+        $meta9 = new ClassMetadata('orm_version');
 
-        $meta = new ClassMetadata('orm_version');
-        $meta->setPrimaryTable(array('name' => 'orm_version'));
+        $meta1->setPrimaryTable(array('name' => 'foo'));
+        $meta2->setPrimaryTable(array('name' => 'orm_version'));
+        $meta3->setPrimaryTable(array('name' => 'orm_version'));
+        $meta4->setPrimaryTable(array('name' => 'orm_version'));
+        $meta5->setPrimaryTable(array('name' => 'orm_version'));
+        $meta6->setPrimaryTable(array('name' => 'orm_version'));
+        $meta7->setPrimaryTable(array('name' => 'orm_version'));
+        $meta8->setPrimaryTable(array('name' => 'orm_version'));
+        $meta9->setPrimaryTable(array('name' => 'orm_version'));
 
-        $config = array();
-        $contaoDca->readExtendedMetadata($meta, $config);
+        $config1 = array();
+        $config2 = array();
+        $config3 = array();
+        $config4 = array();
+        $config5 = array();
+        $config6 = array();
+        $config7 = array();
+        $config8 = array();
+        $config9 = array();
 
-        $this->assertArrayHasKey('create', $config);
-        $this->assertArraySubset(array('createdAt'), $config['create']);
+        try {
+            $driver1->readExtendedMetadata($meta1, $config1);
+        } catch (\Exception $e) {
+            $this->assertEquals('Undefined index: TL_DCA', $e->getMessage());
+        }
+        $this->assertArrayNotHasKey('create', $config1);
+
+        $driver2->readExtendedMetadata($meta2, $config2);
+        $this->assertArrayHasKey('create', $config2);
+        $this->assertArraySubset(array('createdAt'), $config2['create']);
+
+        $this->setUpDataContainerVersion();
+        $driver3->readExtendedMetadata($meta3, $config3);
+        $this->assertArrayHasKey('create', $config3);
+        $this->assertArraySubset(array('createdAt'), $config3['create']);
+
+        $GLOBALS['TL_DCA']['orm_version']['fields']['createdAt']['field']['timestampable']['on'] = 'foo';
+        $driver4->readExtendedMetadata($meta4, $config4);
+        $this->assertArrayNotHasKey('create', $config4);
+
+        unset($GLOBALS['TL_DCA']['orm_version']['fields']['createdAt']['field']['timestampable']['on']);
+        $driver5->readExtendedMetadata($meta5, $config5);
+        $this->assertArrayNotHasKey('create', $config5);
+
+        unset($GLOBALS['TL_DCA']['orm_version']['fields']['createdAt']['field']['timestampable']);
+        $driver6->readExtendedMetadata($meta6, $config6);
+        $this->assertArrayNotHasKey('create', $config6);
+
+        unset($GLOBALS['TL_DCA']['orm_version']['fields']['createdAt']['field']);
+        $driver7->readExtendedMetadata($meta7, $config7);
+        $this->assertArrayNotHasKey('create', $config7);
+
+        unset($GLOBALS['TL_DCA']['orm_version']['fields']['createdAt']);
+        $driver8->readExtendedMetadata($meta8, $config8);
+        $this->assertArrayNotHasKey('create', $config8);
+
+        unset($GLOBALS['TL_DCA']['orm_version']['fields']);
+        try {
+            $driver9->readExtendedMetadata($meta9, $config9);
+        } catch (\Exception $e) {
+            $this->assertEquals('Undefined index: fields', $e->getMessage());
+        }
+        $this->assertArrayNotHasKey('create', $config9);
     }
 
     /**
@@ -58,12 +128,55 @@ class ContaoDcaTest extends BaseTestCase
      */
     public function testSetOriginalDriver()
     {
-        $contaoDca = new ContaoDca();
+        $driver = $this->mockForContaoDriver(true);
 
-        $driver = new ContaoDcaDriver('');
+        $mapping = $this->getMockBuilder('\Contao\Doctrine\ORM\Mapping\Driver\ContaoDcaDriver')
+            ->disableOriginalConstructor()
+            ->getMock();
 
-        $contaoDca->setOriginalDriver($driver);
+        $driver->setOriginalDriver($mapping);
 
-        $this->assertAttributeNotEmpty('originalDriver', $contaoDca);
+        $this->assertAttributeNotEmpty('originalDriver', $driver);
+    }
+
+    /**
+     * Mock the contao data container driver.
+     *
+     * @return ContaoDca|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function mockForContaoDriver($never = false)
+    {
+        $mock = $this
+            ->getMockBuilder(
+                '\Gedmo\Timestampable\Mapping\Driver\ContaoDca'
+            )
+            ->setMethods(array('loadDataContainer'))
+            ->getMock();
+
+        if (false === $never) {
+            $mock->expects($this->once())->method('loadDataContainer')->willReturnCallback(
+                function ($tableName) {
+                    if ('orm_version' !== $tableName) {
+                        return;
+                    }
+
+                    $this->setUpDataContainerVersion();
+                }
+            );
+        }
+
+        if (true === $never) {
+            $mock->expects($this->never())->method('loadDataContainer')->willReturnCallback(
+                function ($tableName) {
+                    if ('orm_version' !== $tableName) {
+                        return;
+                    }
+
+                    $this->setUpDataContainerVersion();
+                }
+            );
+        }
+
+        return $mock;
     }
 }
